aafb230f2eb46aebb1c8541817ac859a9775170f
Anacleto3000 <niltonmaster159@gmail.com>
2025-07-27 16:49:18 -0500

    chore: se ha agregado los archivos iniciales del proyecto

---
+++ RECREACION/index.js
@@ -0,0 +1,16 @@
+function AbrirCarrito() {
+  let ventana_carrito = document.getElementsByClassName("box-carrito")[0];
+  ventana_carrito.style.display = "flex"
+
+}
+
+
+function CerrarCarrito() {
+  let icono_cerrar_carrito = document.getElementsByClassName("box-carrito")[0]
+  icono_cerrar_carrito.style.display = "none"
+}
+
+function AgregarProducto_noti() {
+  let noti_producto_agregado = document.getElementsByClassName("box-principal")[0]
+  noti_producto_agregado.style.display="flex"
+}
